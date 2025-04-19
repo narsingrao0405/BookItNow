44e2c69e61faaf974ddb9af6322480b05204cbd4
Narsing Rao <narsing0405@gmail.com>
2025-04-19 09:04:50 +0530

    feat: enhance user registration and login error handling with success messages

--- src/client/api/index.ts
+++ src/client/api/index.ts
@@ -2,16 +2,20 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8082",
+    //baseURL: "http://localhost:8082",
     headers: {
         "Content-Type": "application/json",
-        "Authorization": token ? `Bearer ${token}` : "",
+        //"Authorization": token ? `Bearer ${token}` : "",
     },
 });
-axiosInstance.interceptors.request.use((config) => {
-    if (!config.headers) {
-        config.headers = {}; // Initialize headers if undefined
+axiosInstance.interceptors.request.use(function (config:any) {
+    
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }, function (error:any) {
+        return Promise.reject(error);
     }
-    config.headers["Authorization"] = token ? `Bearer ${token}` : "";
-    return config;
-});
+);
--- src/client/api/users.ts
+++ src/client/api/users.ts
@@ -9,10 +9,12 @@ export const registerUser = async (values: any) => {
         const response = await axiosInstance.post('/api/users/register', values);
         
         console.log("Register User Response:::::::", response);
+        message.success("User Registered Successfully");
         return response.data;
 
     }catch (error:any) {
         console.error("Error registering user:", { message: error.message, response: error.response?.data, status: error?.response?.status });
+        message.error("Error registering user", error.message);
 
         throw error;
     }
@@ -22,9 +24,11 @@ export const loginUser = async (userData) => {
     try{
         const response = await axiosInstance.post('/api/users/login', userData);
         return response.data;
+        message.success("User Logged In Successfully");
 
     }catch(error){
         console.error("Error logging in User", error);
+        message.error("Error logging in User", error.message);
         throw error;
 
     }
@@ -34,9 +38,11 @@ export const loginUser = async (userData) => {
 export const GetCurrentUser = async () => {
     try{
         const response = await axiosInstance.get('/api/users/get-current-user');
+        message.success("User Login Successfully");
         return response.data;
     }catch(error){
         console.error("Error fetching current user", error);
+        message.error("Error fetching current user", error.message);
         throw error;
     }
 
