f99224589a8e30ae0a91255cf39a6bd38dc39764
Narsing Rao <narsing0405@gmail.com>
2025-04-20 05:46:55 +0530

    chore: update package.json to add new devDependencies and maintain existing ones

--- src/client/api/index.ts
+++ src/client/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const axiosInstance = axios.create({
-    //baseURL: "http://localhost:8082",
+    baseURL: "http://localhost:8082",
     headers: {
         "Content-Type": "application/json",
         //"Authorization": token ? `Bearer ${token}` : "",
